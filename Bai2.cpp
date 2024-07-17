3731f9116f298fd2ac27cd1d1058fa91a63d1f73
A105PC35 <hellow12343@gmail.com>
2024-07-17 16:58:32 +0700

    hoan thanh ham so sanh 2 hon so

--- Bai2.cpp
+++ Bai2.cpp
@@ -26,6 +26,15 @@ void taoMangHonSoNgauNhien(HonSo b[], int n, int min, int max)
 	}
 }
 
+int soSanhHonSo(HonSo hs1, HonSo hs2) {
+	int giaTri1 = hs1.phanNguyen * hs1.mauSo + hs1.tuSo;
+	int giaTri2 = hs2.phanNguyen * hs2.mauSo + hs2.tuSo;
+
+	if (giaTri1 * hs2.mauSo < giaTri2 * hs1.mauSo) return -1;
+	if (giaTri1 * hs2.mauSo > giaTri2 * hs1.mauSo) return 1;
+	return 0;
+}
+
 void xuatMang(HonSo b[], int n)
 {
 	for (int i = 0; i < n; i++)
@@ -52,6 +61,7 @@ int main() {
 		printf("\nMenu:\n");
 		printf("1. Tao mang hon so ngau nhien\n");
 		printf("2. Xuat danh sach hon so\n");
+		printf("3. So sanh 2 hon so duoc chon\n");
 		printf("0. Thoat\n");
 		printf("Hay nhap lua chon: ");
 		scanf("%d", &chon);
@@ -65,6 +75,28 @@ int main() {
 			printf("Danh sach hon so:\n");
 			xuatMang(b, n);
 			break;
+		case 3: 
+		{
+			int i1, i2;
+			printf("Nhap chi so cua hai hon so can so sanh (0 <= x < %d): ", n);
+			scanf("%d %d", &i1, &i2);
+			if (i1 < 0 || i1 >= n || i2 < 0 || i2 >= n) {
+				printf("Chi so khong hop le.\n");
+			}
+			else {
+				int ketQua = soSanhHonSo(b[i1], b[i2]);
+				if (ketQua == 0) {
+					printf("Hai hon so bang nhau.\n");
+				}
+				else if (ketQua < 0) {
+					printf("Hon so thu nhat nho hon hon so thu hai.\n");
+				}
+				else {
+					printf("Hon so thu nhat lon hon hon so thu hai.\n");
+				}
+			}
+			break;
+		}
 		case 0:
 			printf("Thoat chuong trinh.\n");
 			break;
